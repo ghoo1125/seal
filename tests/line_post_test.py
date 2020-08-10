ab760666ea7d03974a8ae7b967b11de296b4b51b
Louis Chang <hanchung.chang@oath.com>
2020-08-10 18:00:13 +0800

    Rename module and add unit tests

---
+++ tests/line_post_test.py
@@ -0,0 +1,21 @@
+from asyncio import coroutine
+
+import pytest
+from fastapi import Request, status
+
+from avbuddy.server import line_post
+
+
+@pytest.mark.asyncio
+async def test_line_post(mocker):
+    mock_request = mocker.patch("fastapi.Request")
+    mock_request.body = mocker.Mock(side_effect=coroutine(
+        lambda: bytes('{"destination": "xxx", "events": []}', 'utf-8')))
+    mocker.patch("avbuddy.server.parser.parse", return_value=[])
+
+    actual = await line_post(mock_request, None)
+    assert actual.status_code == status.HTTP_200_OK
+
+# for debug mode
+if __name__ == "__main__":
+    pytest.main()
