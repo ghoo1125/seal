e01547ac519a8f00266c5a0e84aea4d9fb379ee1
ghoo1125 <ghoo1125@gamil.com>
2021-03-14 18:16:08 +0800

    Implement mongo client

---
+++ seal/db/mongodb_client.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient, errors
+
+
+class MongoClientFactory():
+    __DOMAIN = 'localhost'
+    __PORT = 27017
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if MongoClientFactory.__instance is None:
+            MongoClientFactory.__instance = MongoClientFactory(
+                MongoClientFactory.__instance)
+        return MongoClientFactory.__instance
+
+    def __init__(self, instance):
+        assert(instance == MongoClientFactory.__instance)
+        try:
+            self.client = MongoClient(
+                host=[str(MongoClientFactory.__DOMAIN) + ":" +
+                      str(MongoClientFactory.__PORT)],
+                serverSelectionTimeoutMS=3000,  # 3 second timeout
+            )
+            print("server version:", self.client.server_info()["version"])
+        except errors.ServerSelectionTimeoutError as err:
+            self.client = None
+            print("pymongo ERROR:", err)
